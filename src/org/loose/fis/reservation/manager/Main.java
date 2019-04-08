aa5bd78d12d75b4913d28afd3735927a8830f918
Vilceanu Nicolae <vn.nick02@gmail.com>
2019-04-08 15:22:31 +0300

    Merge branch 'master' into cleanup

--- src/org/loose/fis/reservation/manager/Main.java
+++ src/org/loose/fis/reservation/manager/Main.java
@@ -43,30 +43,5 @@ public class Main {
        return Arrays.asList(reservation1, reservation2, reservation3, reservation4, reservation5, reservation6, reservation7);
    }
 
-
-    public static void main(String[] args) {
-        //creating the users
-    
-        User user1 = new User("John", "Doe", 43);
-        User user2 = new User("Angelina", "Clooney", 25);
-        User user3 = new User("George", "Jackson", 58);
-        User user4 = new User("Francesca", "Jolie", 19);
-
-        //creating reservations
-        Reservation reservation1 = new Reservation(user1, "I would like to make a dentists' appointment.");
-        Reservation reservation2 = new Reservation(user2, "I would like to make a reservation for a nice restaurant.");
-        Reservation reservation3 = new Reservation(user4, "I would like to make an appointment at a nail salon.");
-        Reservation reservation4 = new Reservation(user1, "I would like to make a reservation at the best ice cream place in town.");
-        Reservation reservation5 = new Reservation(user3, "I would like to go to a concert.");
-        Reservation reservation6 = new Reservation(user4, "I would like to get my make up done.");
-        Reservation reservation7 = new Reservation(user2, "I would like to make a reservation at the closest movie theater.");
-    
-        //adding all the reservations to a list
-        List<Reservation> reservations = Arrays.asList(reservation1, reservation2, reservation3, reservation4, reservation5, reservation6, reservation7);
-        
-        //printing out the reservations
-        reservations.forEach(System.out::println);
-    }
-
 }
 
